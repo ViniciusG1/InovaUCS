91003c3a5814fc3a02f94f47cbc50c582e16981c
gustavotonetti <153043620+gustavotonetti@users.noreply.github.com>
2024-05-28 21:18:30 -0300

    Update admin.py

--- website/admin.py
+++ website/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from website.models import *
 from website.modelsViews import *
 
+admin.site.register(Area)
+admin.site.register(SubArea)
 admin.site.register(Pesquisador, PesquisadorAdmin)
 admin.site.register(Instituicao)
 admin.site.register(TipoProducao)
