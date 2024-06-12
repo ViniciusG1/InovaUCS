59df0b54b1bcb93c8bde77f122ec0bbb5f6df05d
ViniciusG1 <148396966+ViniciusG1@users.noreply.github.com>
2024-06-11 21:09:32 -0300

    Update admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from website.models import *
-from website.modelsViews import *
 
+admin.site.register(Pesquisador)
 admin.site.register(Area)
 admin.site.register(SubArea)
-admin.site.register(Pesquisador, PesquisadorAdmin)
 admin.site.register(Instituicao)
-admin.site.register(TipoProducao)
-admin.site.register(Producao)
-admin.site.register(Fomento)
+admin.site.register(FormacaoAcademica)
 admin.site.register(Projeto)
+admin.site.register(Fomento)
