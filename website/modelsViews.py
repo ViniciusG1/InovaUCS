9847223ae2406f11c3a9b6021e63f9cf7ab958ad
ViniciusG1 <148396966+ViniciusG1@users.noreply.github.com>
2024-06-18 20:14:06 -0300

    Update modelsViews.py

--- website/modelsViews.py
+++ website/modelsViews.py
@@ -1,6 +1,1 @@
-from django.contrib import admin
 
-class PesquisadorAdmin(admin.ModelAdmin):
-    list_display = [ 'id', 'nomePesq', 'cpf', 'email', 'formacao' ]
-    search_fields = [ 'nomePesq', 'cpf', 'email' ]
-    list_filter = [ 'formacao' ]
